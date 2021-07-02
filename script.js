934c03b6baec0e7ba31dfa5b3c225f15eb231a22
jcharfauros <julia.charfauros@gmail.com>
2021-07-02 00:21:55 -0400

    update about and art section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('transparent');
-    } else {
-      $('nav').removeClass('transparent');
-    }
-  });
+// $(window).scroll(function() {
+//     if ($(document).scrollTop() > 50) {
+//       $('nav').addClass('transparent');
+//     } else {
+//       $('nav').removeClass('transparent');
+//     }
+//   });
